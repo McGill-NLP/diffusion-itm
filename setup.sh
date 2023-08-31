5e3053b36f1bdce03efd45700a5e49574e982424
BennoKrojer <benno.krojer@gmail.com>
2023-08-31 18:27:13 -0400

    dataset setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Download the files
-# wget -P data/pets https://thor.robots.ox.ac.uk/~vgg/data/pets/images.tar.gz
-# tar -xzf data/pets/images.tar.gz -C data/pets/
-# rm data/pets/images.tar.gz
+wget -P data/pets https://thor.robots.ox.ac.uk/~vgg/data/pets/images.tar.gz
+tar -xzf data/pets/images.tar.gz -C data/pets/
+rm data/pets/images.tar.gz
 
-# wget -P data/clevr https://zenodo.org/record/8096756/files/images.zip
-# unzip data/clevr/images.zip -d data/clevr
-# rm data/clevr/images.zip
+wget -P data/clevr https://zenodo.org/record/8096756/files/images.zip
+unzip data/clevr/images.zip -d data/clevr
+rm data/clevr/images.zip
 
-# wget -P data/imagecode https://zenodo.org/record/6518944/files/image-sets.zip
-# unzip data/imagecode/image-sets.zip -d data/imagecode/
+wget -P data/imagecode https://zenodo.org/record/6518944/files/image-sets.zip
+unzip data/imagecode/image-sets.zip -d data/imagecode/
 
-# python3 data/svo/download.py
+python3 data/svo/download.py
 
 wget -P data/mmbias/data/Images https://github.com/sepehrjng92/MMBias/raw/main/data/Images/Nationality.zip
 unzip data/mmbias/data/Images/Nationality.zip -d data/mmbias/data/Images/
